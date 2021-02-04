109e4511bc742fe8c8db922dd005376205b86074
Ritwik Singh <46321538+ritwiksingh21@users.noreply.github.com>
2021-02-04 21:12:39 +0530

    Update laneDetector.py

--- laneDetector.py
+++ laneDetector.py
@@ -1,23 +1,22 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 def canny(image):
-    grayimg = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blurimg = cv2.GaussianBlur(grayimg,(5,5),0)
-    cannyimg = cv2.Canny(blurimg,50, 100)
-    return cannyimg
+    grayImage = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blurImage = cv2.GaussianBlur(grayImage,(5,5),0)
+    cannyImage = cv2.Canny(blurImage,50, 100)
+    return cannyImage
 
-def areaofinterest(image):
+def areaOfInterest(image):
     height = image.shape[0]
     polygons = np.array([[(200, height), (1100,height), (550, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    maskedimg = cv2.bitwise_and(image, mask)
-    return maskedimg
+    maskedImage = cv2.bitwise_and(image, mask)
+    return maskedImage
 
-def displayline(image, lines):
-    lineimg = np.zeros_like(image)
+def displayLine(image, lines):
+    lineImage = np.zeros_like(image)
     if lines is not None:
         for line in lines:
             x1,y1,x2,y2 = line
@@ -26,23 +25,23 @@ def displayline(image, lines):
         if lines.shape == (2,4):
             
             polygon2 = np.array([[(lines[0,0]+10, lines[0,1]), (lines[0,2]+5,lines[0,3]), (lines[1,2]-5, lines[1,3]), (lines[1,0]-10,lines[1,1])]])
-            cv2.fillPoly(lineimg, polygon2, (50,255,0))
-    return lineimg
+            cv2.fillPoly(lineImage, polygon2, (50,255,0))
+    return lineImage
 
 def status(image, lines):
-    if lines is not None:
-        if lines.shape == (2,4):
-            cv2.rectangle(displayimg,(480,510),(760,580),(50,200,0),-6)
-            cv2.putText(displayimg,"Lane Visibility Good", (500,550),cv2.FONT_HERSHEY_PLAIN,1.5,(0,0,255),2)
+    if averagedLines is not None:
+        if averagedLines.shape == (2,4):
+            cv2.rectangle(image,(480,510),(760,580),(50,200,0),-6)
+            cv2.putText(image,"Lane Visibility Good", (500,550),cv2.FONT_HERSHEY_PLAIN,1.5,(0,0,255),2)
             
         else:
-            cv2.rectangle(displayimg,(480,510),(760,580),(5,0,180),-6)
-            cv2.putText(displayimg,"Low Lane Visibility", (500,550),cv2.FONT_HERSHEY_PLAIN,1.5,(0,255,255),2)
-    return displayimg
+            cv2.rectangle(image,(480,510),(760,580),(5,0,180),-6)
+            cv2.putText(image,"Low Lane Visibility", (500,550),cv2.FONT_HERSHEY_PLAIN,1.5,(0,255,255),2)
+    return image
     
-def averageslopeintercept(image, lines):
-    leftfit = []
-    rightfit = []
+def averageSlopeIntercept(image, lines):
+    leftFit = []
+    rightFit = []
     l=0
     r=0
     for line in lines:
@@ -51,48 +50,49 @@ def averageslopeintercept(image, lines):
         slope = parameters[0]
         intercept = parameters[1 ]
         if slope < 0:
-            leftfit.append((slope, intercept))
+            leftFit.append((slope, intercept))
         else:
-            rightfit.append((slope, intercept))
-    if len(leftfit) > 0:
-        leftfitaverage = np.average(leftfit, axis = 0)
-        leftline = makecoordinates(image, leftfitaverage)
+            rightFit.append((slope, intercept))
+    if len(leftFit) > 0:
+        leftFitAverage = np.average(leftFit, axis = 0)
+        leftLine = makeCoordinates(image, leftFitAverage)
         l=1
-    if len(rightfit) > 0:
-        rightfitaverage = np.average(rightfit, axis = 0)
-        rightline = makecoordinates(image, rightfitaverage)
+    if len(rightFit) > 0:
+        rightFitAverage = np.average(rightFit, axis = 0)
+        rightLine = makeCoordinates(image, rightFitAverage)
         r=1
     
     if l==1 and r==1:
-        return np.array([leftline, rightline])
+        return np.array([leftLine, rightLine])
     elif l==0 and r==1:
-        return np.array([rightline])
+        return np.array([rightLine])
     elif l==1 and r==0:
-        return np.array([lefttline])
+        return np.array([lefttLine])
     
     
-def makecoordinates(image, lineparameters):
-    slope, intercept = lineparameters
+def makeCoordinates(image, lineParameters):
+    slope, intercept = lineParameters
     y1 = image.shape[0]
     y2 = int(y1*(3/6))
     x1 = int((y1 - intercept)/slope)
     x2 = int((y2 - intercept)/slope)
     return np.array([x1, y1, x2, y2])
-    
-cap = cv2.VideoCapture("road.mp4")
+
+cap = cv2.VideoCapture("road.mp4") # <- The Video that we will use
+
 while{cap.isOpened()}:
     _, frame = cap.read()
-    cannyimg = canny(frame)
-    croppedarea = areaofinterest(cannyimg)
+    cannyImage = canny(frame)
+    croppedArea = areaOfInterest(cannyImage)
 
-    lines = cv2.HoughLinesP(croppedarea,2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
-    averagedlines = averageslopeintercept(frame, lines)
-    lineimg = displayline(frame, averagedlines)
+    lines = cv2.HoughLinesP(croppedArea,2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
+    averagedLines = averageSlopeIntercept(frame, lines)
+    lineImage = displayLine(frame, averagedLines)
 
-    displayimg = cv2.addWeighted(frame, 1, lineimg, 0.3, 1)
-    status(displayimg, averagedlines)
+    displayImage = cv2.addWeighted(frame, 1, lineImage, 0.3, 1)
+    status(displayImage, averagedlines)
 
-    cv2.imshow("results", displayimg )
+    cv2.imshow("results", displayImage)
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
